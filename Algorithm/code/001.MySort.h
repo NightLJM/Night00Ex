e31aeb660f6546d5d8a39ee90c26d457eb68eb54
Night00 <2750386116@qq.com>
2021-05-18 22:04:22 +0800

    2021.05.18-company-algorithm-IO

---
+++ Algorithm/code/001.MySort.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iterator>
+#include <type_traits>
+
+namespace Night00
+{
+    template<typename ForwardIterator, typename CompFun>
+    void bubbleSort(ForwardIterator beginIter, ForwardIterator endIter, CompFun comFun);
+
+    
+
+}
